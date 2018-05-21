18e009b4abe1c37f2529757ca73fd89c5cb4ad87
nusjzx <nusjzx@gmail.com>
2018-05-21 15:02:39 +0800

    Add searchbar to extract the matching search result only

---
+++ src/searchbar.vue
@@ -0,0 +1,16 @@
+<script>
+    import Typeahead from './Typeahead.vue'
+    export default {
+        extends: Typeahead,
+        filters: {
+            highlightAndExtract (value, phrase) {
+                var words = value.split(',').map(value => value.trim());
+                return words.reduce((pre, next) => {
+                    var regex = new RegExp('(' + phrase + ')', 'gi');
+                    var isMatch = next.match(regex);
+                    return pre + (isMatch? next.replace(regex, '<mark>$1</mark>') + ' ': '');
+                }, '');
+            }
+        }
+    }
+</script>
